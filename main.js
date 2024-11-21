1e8037b41980aecd7153f614ba5ed59b2158f9f1
elizeu <elizeu@teste.com>
2024-11-21 11:36:28 -0300

    fix limitaão casa decimais

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function atualizaTabela() {
 
 function atualizaMediaFinal() {
     const mediaFinal = calcularMediaFinal();
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(1);
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
